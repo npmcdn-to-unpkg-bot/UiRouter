f69767d5802a4d0a9ec66fb78bac8b7387537682
bambus.ca@gmail.com <bambus.ca@gmail.com>
2016-04-23 17:56:15 -0700

    added ode

---
+++ UiRouter/app/scripts/ode/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module("routedTabs", ["ui.router"]);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/main/tab1");
+
+    $stateProvider
+        .state("main", { abstract: true, url: "/main", templateUrl: "app/templates/ode/main.html" })
+            .state("main.tab1", { url: "/tab1", templateUrl: "app/templates/ode/tab1.html" })
+            .state("main.tab2", { url: "/tab2", templateUrl: "app/templates/ode/tab2.html" })
+            .state("main.tab3", { url: "/tab3", templateUrl: "app/templates/ode/tab3.html" });
+});
---
+++ UiRouter/app/scripts/ode/main-controller.js
@@ -0,0 +1,24 @@
+﻿angular
+    .module("routedTabs")
+    .controller("mainController", function ($rootScope, $scope, $state) {
+
+    $scope.tabs = [
+        { heading: "Tab 1", route: "main.tab1", active: false },
+        { heading: "Tab 2", route: "main.tab2", active: false },
+        { heading: "Tab 3", route: "main.tab3", active: false },
+    ];
+
+    $scope.go = function (route) {
+        $state.go(route);
+    };
+
+    $scope.active = function (route) {
+        return $state.is(route);
+    };
+
+    $scope.$on("$stateChangeSuccess", function () {
+        $scope.tabs.forEach(function (tab) {
+            tab.active = $scope.active(tab.route);
+        });
+    });
+});
