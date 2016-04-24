36b1c542bfd2f30b17c15b08538425a060295659
bambus.ca@gmail.com <bambus.ca@gmail.com>
2016-04-24 09:48:55 -0700

    added angular ui

--- UiRouter/app/scripts/ode/app.js
+++ UiRouter/app/scripts/ode/app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module("routedTabs", ["ui.router"]);
+﻿var app = angular.module("routedTabs", ["ui.router", "ui.bootstrap"]);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
 
---
+++ UiRouter/app/scripts/test-ui/app.js
@@ -0,0 +1,3 @@
+﻿angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('test', function ($scope) {
+});
