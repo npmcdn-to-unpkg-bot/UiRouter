16a53c9bebaf0d08dfa08203c18f35b847a7baf8
bambus.ca@gmail.com <bambus.ca@gmail.com>
2016-04-28 21:28:36 -0700

    code refactoring

--- UiRouter/app/scripts/test-ui/app.js
+++ UiRouter/app/scripts/test-ui/app.js
@@ -1,3 +1,3 @@
-﻿angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('test', function ($scope) {
+﻿var app = angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
+app.controller('test', function ($scope) {
 });
