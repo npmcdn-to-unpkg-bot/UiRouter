121d00245eb601f3b31d249cfe81dd44da172a54
bambus.ca@gmail.com <bambus.ca@gmail.com>
2016-05-04 19:32:13 -0700

    renamed

--- UiRouter/app/scripts/nested/app.js
+++ UiRouter/app/scripts/nested/app.js
@@ -3,20 +3,20 @@ example.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('settings', {
             url: '/settings',
-            templateUrl: 'app/templates/multi/settings.html'
+            templateUrl: 'app/templates/nested/settings.html'
         })
         .state('settings.profile', {
             url: '/profile',
-            templateUrl: 'app/templates/multi/profile.html',
+            templateUrl: 'app/templates/nested/profile.html',
             controller: 'ProfileController'
         })
         .state('settings.account', {
             url: '/account',
-            templateUrl: 'app/templates/multi/account.html',
+            templateUrl: 'app/templates/nested/account.html',
             controller: 'AccountController'
         }).state('settings.tab3', {
             url: '/tab3',
-            templateUrl: 'app/templates/multi/tab3.html',
+            templateUrl: 'app/templates/nested/tab3.html',
             controller: 'Tab3Controller'
         });
     $urlRouterProvider.otherwise('/settings/profile');
