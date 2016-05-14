5eddd7b67d022d39892c580dd38b0743ec005c0b
bambus.ca@gmail.com <bambus.ca@gmail.com>
2016-05-14 13:03:24 -0700

    added app.js

---
+++ UiRouter/app-cr/demo/app.js
@@ -0,0 +1,8 @@
+﻿angular.module('app', ['ngComponentRouter'])
+    .config(function ($locationProvider) {
+        $locationProvider.html5Mode(true);
+    })
+    .value('$routerRootComponent', 'app')
+    .component('app', {
+        template: 'It worked!'
+    });
