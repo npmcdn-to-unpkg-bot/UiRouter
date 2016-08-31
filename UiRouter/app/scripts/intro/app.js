efcf149c79dd27dc25e47b6609a444e1e63e8349
bambus.ca@gmail.com <bambus.ca@gmail.com>
2016-08-30 18:51:58 -0700

    app-intro

--- UiRouter/app/scripts/intro/app.js
+++ UiRouter/app/scripts/intro/app.js
@@ -2,19 +2,21 @@
     .module("app", ["ui.router"])
     .config(["$urlRouterProvider", "$stateProvider", function($urlRouterProvider, $stateProvider) {
         $urlRouterProvider.otherwise("/home");
-        $stateProvider.state("home", {
-            url: "/home",
-            templateUrl: "app/templates/intro/home.html",
-            controller: "homeController",
-            resolve: {
-                friends: [
-                    "Friends", function (Friends) {
-                        return Friends.get();
-                    }]
-            }
-        }).state("about", {
-            url: "/about",
-            templateUrl: "app/templates/intro/about.html",
-            controller: "aboutController"
-        });
+        $stateProvider
+            .state("home", {
+                url: "/home",
+                templateUrl: "app/templates/intro/home.html",
+                controller: "homeController",
+                resolve: {
+                    friends: [
+                        "Friends", function (Friends) {
+                            return Friends.get();
+                        }]
+                }
+            })
+            .state("about", {
+                url: "/about",
+                templateUrl: "app/templates/intro/about.html",
+                controller: "aboutController"
+            });
     }]);
